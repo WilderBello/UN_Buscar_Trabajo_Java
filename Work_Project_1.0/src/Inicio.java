6287f6e02c5832ea43ae9a21b71565f4c6947a91
WilderBello <wobelloh@unal.edu.co>
2017-05-14 11:27:05 -0500

    ...

---
+++ Work_Project_1.0/src/Inicio.java
@@ -0,0 +1,13 @@
+import processing.core.*;
+
+public class Inicio extends PApplet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public void settings(){
+		size(600,600);
+	}
+
+}
