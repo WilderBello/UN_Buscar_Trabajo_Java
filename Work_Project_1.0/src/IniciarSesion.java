e48fde6b3dacce3efc22eb4719ce576309e5cd8a
WilderBello <wobelloh@unal.edu.co>
2017-05-17 09:26:42 -0500

    agregado inicio beta

--- Work_Project_1.0/src/IniciarSesion.java
+++ Work_Project_1.0/src/IniciarSesion.java
@@ -4,6 +4,9 @@ import controlP5.*;
 
 public class IniciarSesion extends PApplet{
 	
+	ControlP5 cp5;
+	String textValue = "";
+	
 	public static void main(String[] args) {
 		PApplet.main("IniciarSesion");
 	}
@@ -13,6 +16,60 @@ public class IniciarSesion extends PApplet{
 	/*TODO Solicitar datos al usuario: Usuario y contraseña
 	 * 
 	 */
+	public void setup() {
+		  PFont font = createFont("arial",20);
+		  
+		  cp5 = new ControlP5(this);
+		  //Label Usuario
+		  cp5.addTextfield("User")
+		     .setPosition(100,100)
+		     .setSize(200,30)
+		     .setFont(font)
+		     .setFocus(true)
+		     .setColor(color(255,0,0))
+		     .setAutoClear(false)
+		     ;
+		//Label Contraseña              
+		  cp5.addTextfield("Password")
+		     .setPosition(100,170)
+		     .setSize(200,30)
+		     .setFont(createFont("arial",30))
+		     .setAutoClear(false)
+		     ;
+		  //TODO Botón    
+		  cp5.addBang("Ingresar")
+		     .setPosition(160,240)
+		     .setSize(80,40)
+		     .getCaptionLabel().align(ControlP5.CENTER, ControlP5.CENTER)
+		     ; 
+		     
+		  textFont(font);
+		}
 	
+	public void draw() {
+		  background(150);
+		  fill(255);
+		  text(cp5.get(Textfield.class,"User").getText(), 360,130);
+		  text(textValue, 360,180);
+		}
+
+	public void clear() {
+		  cp5.get(Textfield.class,"Password").clear();
+		}
+
+	public void controlEvent(ControlEvent theEvent) {
+		if(theEvent.isAssignableFrom(Textfield.class)) {
+			println("controlEvent: accessing a string from controller '"
+		            +theEvent.getName()+"': "
+		            +theEvent.getStringValue()
+		            );
+		  }
+		}
+
+
+		public void input(String theText) {
+		  // automatically receives results from controller input
+		  println("a textfield event for controller 'User' : "+theText);
+		}
 	
 }
