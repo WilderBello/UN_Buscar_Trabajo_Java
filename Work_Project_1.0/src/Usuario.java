077b47ccea370e948532f123f0efe764be3fbf46
WilderBello <wobelloh@unal.edu.co>
2017-05-24 09:48:21 -0500

    Probando BuferedReader

--- Work_Project_1.0/src/Usuario.java
+++ Work_Project_1.0/src/Usuario.java
@@ -1,18 +1,22 @@
 import processing.core.*;
-import javax.*;
-public class Usuario extends PApplet{
+import java.io.*;
+
+public class Usuario extends PApplet {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		PApplet.main("Usuario");
-		println("Funcionando Usuario");
-		inicio();
+		println("Funcionando Usuario"); 
 	}
-	//Bufferedreader <----- Buscar, así se solicita datos.
-	
-	public static void inicio(){
+	//BufferedReader <----- Buscar, así se solicita datos.
+	public static void inicio() throws IOException {
 		//que el usuario decida si quiere crear un usuario o si quiere ingresar 
-		println("Corriendo inicio");
+		println("Bienvenido Usuario"); 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Por favor ingrese su nombre:");
+		String nombre = br.readLine();		
+		System.out.println("Por favor ingrese su edad:");
+		String edad = br.readLine();
+		System.out.println(nombre+": " + edad);
 	}
 	public void crear(){
 		//Solicitar datos al usuario: nickname, contraseña, nombre, apellido, carrera.
