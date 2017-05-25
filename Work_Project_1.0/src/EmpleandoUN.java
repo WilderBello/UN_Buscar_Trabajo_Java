553efff6d3204a51facad7ca5ed78ab228b1f302
WilderBello <wobelloh@unal.edu.co>
2017-05-24 19:19:38 -0500

    Modificando

--- Work_Project_1.0/src/EmpleandoUN.java
+++ Work_Project_1.0/src/EmpleandoUN.java
@@ -1,13 +1,72 @@
-import processing.core.*;
+import controlP5.ControlP5;
+import processing.core.PApplet;
+import processing.core.PFont;
 
-public class Inicio extends PApplet {
 
+public class EmpleandoUN extends PApplet {
+	static ControlP5 inicio;
+	ControlP5 cp5;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		PApplet.main("EmpleandoUN");
 	}
+	
 	public void settings(){
-		size(600,600);
+		size(400,400);
+	}
+	
+	public void setup() {
+	  noStroke();
+	  inicio = new ControlP5(this);
+	  inicio.addButton("ingresar").setPosition(100,200).setSize(80,40).show();
+	  inicio.addButton("nuevo").setPosition(200,200).setSize(80,40).show(); 
+	  cp5 = new ControlP5(this);
+	}
+	
+	public void draw() {
+		background(180);
+	}
+	
+	public void nuevo(){
+		PFont fonter = createFont("arial",20);
+		Usuario.crear(cp5,fonter);
+	}
+	
+	public void ingresar(){
+		PFont fonter = createFont("arial",20);
+		Usuario.init(cp5,fonter);
+	}
+	
+	
+	
+	public void volver(){
+		cp5.addButton("ingresar").hide();
+		cp5.addButton("clear").hide();
+		cp5.addButton("aceptar").hide();
+		cp5.addButton("volver").hide();
+		cp5.addButton("acceder").hide();
+		cp5.addTextfield("user").hide();
+		cp5.addTextfield("password").hide();
+		cp5.addTextfield("nombre").hide();
+		cp5.addTextfield("apellido").hide();
+		cp5.addScrollableList("carrera").hide();
+		cp5.addTextlabel("label").hide();
+		inicio.show();
+	}
+	
+	public void cerrar(){
+		exit();
+	}
+	
+	public void acceder(){
+		PFont fonter = createFont("arial",20);
+		Convocatoria.ofert(cp5,fonter);
+	}
+	
+	public void aceptar(){
+		PFont fonter = createFont("arial",20);
+		Convocatoria.ofert(cp5,fonter);
 	}
 
 }
+
--- Work_Project_1.0/src/Usuario.java
+++ Work_Project_1.0/src/Usuario.java
@@ -1,31 +1,68 @@
 import processing.core.*;
-import java.io.*;
+import java.util.*;
+import controlP5.*;
 
-public class Usuario extends PApplet {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		println("Funcionando Usuario"); 
+public class Usuario extends EmpleandoUN{
+	
+	public static boolean init (ControlP5 cp5,PFont font) {
+		inicio.hide();
+		cp5.addTextfield("user").setPosition(100,100).setSize(200,30).setFont(font).setAutoClear(false);     
+		cp5.addTextfield("password").setPosition(100,170).setSize(200,30).setFont(font).setAutoClear(false);
+		//TODO Botón    
+		cp5.addButton("acceder").setPosition(110,240).setSize(80,40);
+		cp5.addButton("volver").setPosition(210,240).setSize(80,40);
+		return true;
 	}
-	//BufferedReader <----- Buscar, así se solicita datos.
-	public static void inicio() throws IOException {
-		//que el usuario decida si quiere crear un usuario o si quiere ingresar 
-		println("Bienvenido Usuario"); 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		System.out.println("Por favor ingrese su nombre:");
-		String nombre = br.readLine();		
-		System.out.println("Por favor ingrese su edad:");
-		String edad = br.readLine();
-		System.out.println(nombre+": " + edad);
+	
+	public static boolean crear (ControlP5 cp5,PFont font) {
+		inicio.hide();
+		String textVal = "";
+		Textlabel Titulo;
+		Titulo = cp5.addTextlabel("label").setText("CREAR NUEVO USUARIO").setPosition(70,20).setFont(font);
+		cp5.addTextfield("user").setPosition(100,60).setSize(200,30).setFont(font).setAutoClear(false);      
+		cp5.addTextfield("password").setPosition(100,95+15).setSize(200,30).setFont(font).setAutoClear(false);
+		cp5.addTextfield("nombre").setPosition(100,145+15).setSize(200,30).setFont(font).setAutoClear(false);
+		cp5.addTextfield("apellido").setPosition(100,195+15).setSize(200,30).setFont(font).setAutoClear(false);
+		cp5.addButton("aceptar").setPosition(110,305+15).setSize(80,40);
+		cp5.addButton("volver").setPosition(210,305+15).setSize(80,40);		  
+		cp5.addButton("clear").setPosition(340,365).setSize(50,25);	
+		List l = Arrays.asList("Arquitectura","Artes Plásticas", "Cine y Televisión",
+			  "Diseño Gráfico","Diseño Industrial","Música","Música Instrumental", 
+			  "Biología","Estadística","Farmacia","Física","Geología","Matemáticas",
+			  "Química","Administración de Empresas","Contaduría Pública","Economía",
+			  "Antropología","Español y Filología Clásica","Estudios Literarios",
+			  "Filología e Idiomas (Alemán)","Filología e Idiomas (Francés)","Filología e Idiomas (Inglés)",
+			  "Filosofía","Geografía","Historia","Lingüística","Psicología","Sociología",
+			  "Trabajo Social","Ciencia Política","Derecho","Enfermería","Ingeniería Agrícola",
+			  "Ingeniería Civil","Ingeniería de Sistemas y Computación","Ingeniería Eléctrica",
+			  "Ingeniería Electrónica","Ingeniería Industrial","Ingeniería Mecánica","Ingeniería Mecatrónica",
+			  "Ingeniería Química","Fisioterapia","Fonoaudiología","Medicina","Nutrición y Dietética",
+			  "Terapia Ocupacional","Medicina Veterinaria","Zootecnia","Odontología");
+	  /* add a ScrollableList, by default it behaves like a DropdownList */
+		cp5.addScrollableList("carrera").setPosition(100,260).setSize(200, 100).setBarHeight(30).setItemHeight(30).addItems(l);
+		return true;
 	}
-	public void crear(){
-		//Solicitar datos al usuario: nickname, contraseña, nombre, apellido, carrera.
-		println("crear");
+	
+	public void clear() {
+		cp5.get(Textfield.class,"user").clear();
+		cp5.get(Textfield.class,"password").clear();
+		cp5.get(Textfield.class,"nombre").clear();
+	  	cp5.get(Textfield.class,"apellido").clear();
 	}
-
-	public void ingresar(){
-		//Solicitar al usuario: nickname, contraseña.
-		println("Ingresar");
+	
+	public void input(String theText) {
+		  // automatically receives results from controller input
+		  println("a textfield event for controller 'user' : "+theText);
+	}
+	
+	public void controlEvent(ControlEvent theEvent) {
+		if(theEvent.isAssignableFrom(Textfield.class)) {
+			println("controlEvent: accessing a string from controller '"
+					+theEvent.getName()+"': "
+					+theEvent.getStringValue()
+					);
+	  }
 	}
 
 }
